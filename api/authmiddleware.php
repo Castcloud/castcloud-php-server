998af2075a284ac110e3fe5ce61a1c952f8e53a7
Vegard Langas <sjefen6@gmail.com>
2014-02-28 10:41:11 +0000

    Now updates Clientauths SeenTS

--- api/authmiddleware.php
+++ api/authmiddleware.php
@@ -12,6 +12,13 @@ class AuthMiddleware extends \Slim\Middleware {
 					if ($sth && $sth->rowCount() < 1) {
 						json(array("status" => "Bad token"));
 						$this->app->stop();
+					} else {
+						
+						$result = $sth->fetch(PDO::FETCH_ASSOC);
+						$sth = $GLOBALS['dbh'] -> prepare("UPDATE clientauthorization SET SeenTS=:time WHERE UniqueClientID=:UniqueClientID");
+						$sth -> bindParam(':time', time(), PDO::PARAM_INT);
+						$sth -> bindParam(':UniqueClientID', $result["UniqueClientID"], PDO::PARAM_INT);
+						$sth -> execute();
 					}
 				}
 				else {
