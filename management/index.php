f742f1d88ea7ec5dd14f916ae0d201c4deadcfa9
Ken-Håvard Lieng <kk_998@hotmail.com>
2014-03-04 18:28:48 +0100

    Login and basic template structure

--- management/index.php
+++ management/index.php
@@ -1,2 +1,42 @@
 <?php
-//TODO; Management
+require '../lib/Slim/Slim.php';
+\Slim\Slim::registerAutoloader();
+
+session_cache_limiter(false);
+session_start();
+
+$app = new \Slim\Slim();
+GLOBAL $app;
+
+include '../api/cc-settings.php';
+GLOBAL $dbh;
+
+$app->get('/', function() {
+	if (isset($_SESSION['login'])) {
+		$status = "Hai ".$_SESSION['username'];
+	}
+	include('templates/login.phtml');
+});
+
+$app->post('/login', function() use($app) {
+	$username = $app->request->params('username');
+	$password = $app->request->params('password');
+
+	$dbh = $GLOBALS['dbh'];
+	$sth = $dbh -> query("SELECT * FROM users WHERE username='$username'");
+	if ($sth) {
+		if ($result = $sth -> fetch(PDO::FETCH_ASSOC)) {
+			$userid = $result['UserID'];
+			$salt = $result['Salt'];
+			if ($result['Password'] == md5($password . $salt)) {
+				$_SESSION['login'] = true;
+				$_SESSION['username'] = $username;
+			}
+		}
+	}
+
+	$app->response->redirect($_SERVER['HTTP_REFERER']);
+});
+
+$app->run();
+?>
