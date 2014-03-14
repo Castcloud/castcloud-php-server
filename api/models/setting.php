b22ff525e156a060ffdee83f1471637e1fd8a01a
Vegard Langas <sjefen6@gmail.com>
2014-03-14 13:10:53 +0000

    And here comes settings.php

---
+++ api/models/setting.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @SWG\Model(id="setting",required="setting, value, clientts")
+ */
+class setting
+{
+    /**
+     * @SWG\Property(name="settingid",type="string",description="The settings individual ID. Used for deleting settings.")
+     */
+    public $settingid;
+	
+    /**
+     * @SWG\Property(name="setting",type="string",description="Setting string. Identifies what the setting relates to.")
+     */
+    public $setting;
+
+    /**
+     * @SWG\Property(name="value",type="string",description="The value of the setting")
+     */
+    public $value;
+
+    /**
+     * @SWG\Property(name="clientspesific",type="boolean",description="Whether the setting is clientspesific or not.")
+     */
+    public $clientspesific;
+
+}
