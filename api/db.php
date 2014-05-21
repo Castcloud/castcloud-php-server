89f6905bd39353b7e4ed7c87f469ae617389a856
Vegard Langas <sjefen6@gmail.com>
2014-05-21 12:54:49 +0000

    --debug

--- api/db.php
+++ api/db.php
@@ -445,13 +445,8 @@ class DB {
 		$dbh = $GLOBALS['dbh'];
 		$dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, FALSE);
 		$sth = $dbh -> prepare($query);
-		
-		$t = microtime(true);
-
 		$sth->execute($inputs);
-
-		echo microtime(true) - $t;
-
+		
 		if ($sth) {
 			if ($limit == "1" && $sth->rowCount() == 1){
 				$events = $sth->fetchAll(PDO::FETCH_CLASS, "event");
